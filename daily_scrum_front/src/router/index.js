982d84bb89b4d944418afc969d1a84d2fb33023d
YunitaDwiAmbarwati <yunita_dwi_27rpl@student.smktelkom-mlg.sch.id>
2020-04-02 21:34:12 +0700

    fetching api login & regis

--- daily_scrum_front/src/router/index.js
+++ daily_scrum_front/src/router/index.js
@@ -1,27 +1,59 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import store from '../store'
+import Navbar from '../views/layouts/Navbar.vue'
+import Input from '../views/Input.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/login',
+        name: 'login',
+        components: { default: Login }
+
+    },
+    {
+        path: '/register',
+        name: 'register',
+        components: { default: Register },
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/input',
+        name: 'input',
+        components: { default: Input, header: Navbar },
+        meta: {
+            requiresAuth: true
+        }
+    },
+
+
+
+
+
 ]
 
 const router = new VueRouter({
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (store.getters.isLoggedIn) {
+            next()
+            return
+        }
+        next('/login')
+    } else {
+        next()
+    }
 })
 
 export default router
