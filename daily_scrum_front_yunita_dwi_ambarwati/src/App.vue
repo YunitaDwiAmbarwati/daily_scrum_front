98ec8c9d5b6db671fd6dc67c5fed8ac401130755
YunitaDwiAmbarwati <yunita_dwi_27rpl@student.smktelkom-mlg.sch.id>
2020-04-02 21:42:16 +0700

    fetching api login regis

---
+++ daily_scrum_front_yunita_dwi_ambarwati/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+      <div class="container-scroller">
+    <router-view name="header" />
+      <div class="wrapper">
+        <router-view />
+      </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './views/layouts/Navbar'
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  },
+  data : function(){
+    return {
+      status_login: ''
+    }
+  }
+}
+</script>
+
---
+++ daily_scrum_front_yunita_dwi_ambarwati/src/router/index.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import store from '../store'
+import Navbar from '../views/layouts/Navbar.vue'
+import Input from '../views/Input.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/login',
+        name: 'login',
+        components: { default: Login }
+
+    },
+    {
+        path: '/register',
+        name: 'register',
+        components: { default: Register },
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/input',
+        name: 'input',
+        components: { default: Input, header: Navbar },
+        meta: {
+            requiresAuth: true
+        }
+    },
+
+
+
+
+
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (store.getters.isLoggedIn) {
+            next()
+            return
+        }
+        next('/login')
+    } else {
+        next()
+    }
+})
+
+export default router
