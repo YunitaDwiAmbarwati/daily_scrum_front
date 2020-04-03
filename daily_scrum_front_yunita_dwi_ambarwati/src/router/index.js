1ef97cc3b8cfb67a21a147421ec4f3a5de3caf5b
YunitaDwiAmbarwati <yunita_dwi_27rpl@student.smktelkom-mlg.sch.id>
2020-04-03 19:45:42 +0700

    add component

--- daily_scrum_front_yunita_dwi_ambarwati/src/router/index.js
+++ daily_scrum_front_yunita_dwi_ambarwati/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import store from '../store'
 import Navbar from '../views/layouts/Navbar.vue'
+import Daily from '../views/Daily.vue'
 import Input from '../views/Input.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
@@ -19,17 +20,25 @@ const routes = [{
         path: '/register',
         name: 'register',
         components: { default: Register },
-        meta: {
-            requiresAuth: true
-        }
+        // meta: {
+        //     requiresAuth: true
+        // }
+    },
+    {
+        path: '/daily',
+        name: 'daily',
+        components: { default: Daily, header: Navbar },
+        // meta: {
+        //     requiresAuth: true
+        // }
     },
     {
         path: '/input',
         name: 'input',
         components: { default: Input, header: Navbar },
-        meta: {
-            requiresAuth: true
-        }
+        // meta: {
+        //     requiresAuth: true
+        // }
     },
 
 
--- daily_scrum_front_yunita_dwi_ambarwati/src/views/Input.vue
+++ daily_scrum_front_yunita_dwi_ambarwati/src/views/Input.vue
@@ -8,73 +8,55 @@
                 <div class="card-body">
                   <p class="card-title float-left"><i class="mdi mdi-format-list-bulleted menu-icon"></i></p>
                   <p class="card-description float-right">
-                    <a href="#" class="btn btn-sm btn-success btn-icon-text" data-toggle="modal" data-target="#modalDailyScrum">
-                      <i class="mdi mdi-plus btn-icon-prepend"></i>
-                      Add Activity
-                    </a>
+                    <b-button variant="success" v-b-modal.modalDailyScrum v-on:click="Add"><i class="mdi mdi-plus btn-icon-prepend"></i>Add activity</b-button>
                   </p>
                   <div class="table-responsive">
-                    <table class="table table-striped">
-                      <thead>
-                        <tr>
-                          <th>Tanggal</th>
-                          <th>Yesterday</th>
-                          <th>Today</th>
-                          <th>Problem</th>
-                          <th>Solution</th>
-                          <th>Action</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>Senin, 30 Mar 2020</td>
-                          <td>Membuat desain layout aplikasi iklan baris</td>
-                          <td>Membuat API aplikasi daily scrum</td>
-                          <td>Desain layout masih belum rapi</td>
-                          <td>Menambah waktu pengerjaan untuk merapikan layout</td>
-                          <td>
-                            <a href="#" class="btn btn-sm btn-danger">
-                              <i class="mdi mdi-delete btn-icon-prepend"></i>
-                            </a>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>Selasa, 31 Mar 2020</td>
-                          <td>Membuat desain layout aplikasi iklan baris</td>
-                          <td>Membuat API aplikasi daily scrum</td>
-                          <td>Desain layout masih belum rapi</td>
-                          <td>Menambah waktu pengerjaan untuk merapikan layout</td>
-                          <td>
-                            <a href="#" class="btn btn-sm btn-danger">
-                              <i class="mdi mdi-delete btn-icon-prepend"></i>
-                            </a>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
+                    <div class="table-responsive">
+                <b-table striped hover :items="daily" :fields="fields">
+                  <template v-slot:cell(role)="data">
+                    <b-badge variant="warning">{{ data.item.team }}</b-badge>
+                  </template>
+                  <template v-slot:cell(Aksi)="data">
+                    <b-button size="sm" variant="danger" v-on:click="Drop(data.item.id)" v-b-modal.modalDailyScrum><i class="mdi mdi-delete btn-icon-prepend"></i> Hapus</b-button>&nbsp;
+                  </template>
+                </b-table>
+                <b-pagination
+                  v-model="currentPage"
+                  :per-page="perPage"
+                  :total-rows="rows"
+                  align="center"
+                  v-on:input="pagination">
+                </b-pagination>
+
+                <b-toast id="loadingToast" title="Processing Data" no-auto-hide>
+                  <b-spinner label="Spinning" variant="success"></b-spinner>
+                  <strong class="text-success">Loading...</strong>
+                </b-toast>
+
+                <!-- toast untuk tampilan message box -->
+                <b-toast id="message" title="Message">
+                  <strong class="text-success">{{ message }}</strong>
+                </b-toast>
+
               </div>
             </div>
           </div>
         </div>
-        
+      </div>
+    </div>
 
-        <div class="modal fade" id="modalDailyScrum" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content modal-md">
-              <div class="modal-header">
-                <h5 class="modal-title" id="ModalLabel">Add Activity</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <form>
-                  <div class="form-group">
-                    <label for="id_siswa" class="col-form-label">Your Team</label>
-                    <select class="form-control" name="team" id="team">
-                      <option value="BEON" checked>BEON</option>
+    <b-modal 
+      id="modalDailyScrum"
+      @ok="Save"
+    >
+      <template v-slot:modal-title>
+        Add activity
+      </template>
+        <form ref="form">
+          <div class="form-group">
+             <label for="id_users" class="col-form-label">Your Team</label>
+            <select class="form-control" name="team" id="team" placeholder="BEON" v-model="team">
+           <option value="BEON" checked>BEON</option>
                       <option value="DDS">DDS</option>
                       <option value="DOT">DOT</option>
                       <option value="node1">Node 1</option>
@@ -84,38 +66,178 @@
                       <option value="laravel">Laravel</option>
                       <option value="laravel_vue">Laravel & Vue</option>
                       <option value="android">Android</option>
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="activity_yesterday" class="col-form-label">Yesterday Activity</label>
-                    <textarea name="activity_yesterday" id="activity_yesterday" class="form-control" rows="7" cols="10"></textarea>
-                  </div>
-                  <div class="form-group">
-                    <label for="activity_today" class="col-form-label">Today Activity</label>
-                    <textarea name="activity_today" id="activity_today" class="form-control" rows="7" cols="10"></textarea>
-                  </div>
-                  <div class="form-group">
-                    <label for="problem_yesterday" class="col-form-label">Yesterday Problem</label>
-                    <textarea name="problem_yesterday" id="problem_yesterday" class="form-control" rows="7" cols="10"></textarea>
-                  </div>
-                  <div class="form-group">
-                    <label for="solution" class="col-form-label">Your Solution</label>
-                    <textarea name="solution" id="solution" class="form-control" rows="7" cols="10"></textarea>
-                  </div>
-                  <div class="form-group">
-                    <button type="button" class="btn btn-md btn-success">Submit</button>
-                    <button type="button" class="btn btn-md btn-light" data-dismiss="modal">Cancel</button>
-                  </div>
-                </form>
-              </div>
-              <div class="modal-footer">
-                
-              </div>
-            </div>
+            </select>
           </div>
-        </div>
-      </div>
-    </div>
-    
-
+           <div class="form-group">
+             <label for="activity_yesterday" class="col-form-label">Yesterday Activity</label>
+            <input type="text" name="activity_yesterday" class="form-control" rows="7" cols="10" id="activity_yesterday"  v-model="activity_yesterday">
+          </div>
+          <div class="form-group">
+             <label for="activity_today" class="col-form-label">Today Activity</label>
+            <input type="text" name="activity_today" class="form-control" rows="7" cols="10" id="activity_today"  v-model="activity_today">
+          </div>
+           <div class="form-group">
+             <label for="problem_yesterday" class="col-form-label">Yesterday Problem</label>
+            <input type="text" name="problem_yesterday" class="form-control" rows="7" cols="10" id="problem_yesterday"  v-model="problem_yesterday">
+          </div>
+           <div class="form-group">
+             <label for="solution" class="col-form-label">Your Solution</label>
+            <input type="text" name="solution" class="form-control" rows="7" cols="10" id="solution"  v-model="solution">
+          </div>
+        </form>
+    </b-modal>
+</div>
+  </div>
+  </div>
 </template>
+
+
+     
+
+<script>
+
+module.exports = {
+  data : function(){
+    return {
+      search: "",
+      id: "",
+      team: "",
+      activity_yesterday: "",
+      activity_today: "",
+      problem_yesterday: "",
+      solution: "",
+      action: "",
+      message: "",
+      currentPage: 1,
+      rows: 0,
+      perPage: 10,
+      key: "",
+      daily: [],
+      fields: [ "team", "Activity_yesterday", "Activity_today", "Problem_yesterday", "Solution", "Action"], //ngelink ke navbar table bawah pcc
+    }
+  },
+
+  methods: {
+    getData : function(){
+      let conf = { headers: { "Authorization" : 'Bearer ' + this.key } };
+      let offset = (this.currentPage - 1) * this.perPage;
+      this.$bvToast.show("loadingToast");
+      this.axios.get("/daily/" + this.perPage + "/" + offset, conf)
+      .then(response => {
+        if(response.data.status == 1){
+          this.$bvToast.hide("loadingToast");
+          this.daily = response.data.daily;
+          this.rows = response.data.count;
+        } else {
+          this.$bvToast.hide("loadingToast");
+          this.message = "Gagal menampilkan data daily."
+          this.$bvToast.show("message");
+          this.$router.push({name: "login"})
+        }
+        
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
+
+    pagination : function(){
+      if(this.search == ""){
+        this.getData();
+      } else {
+        this.searching();
+      }
+    },
+
+    Add : function(){
+      this.action = "insert";
+      this.id = "";
+      this.team = "";
+      this.activity_yesterday = "";
+      this.activity_today = "";
+      this.problem_yesterday = "";
+      this.solution = "";
+    },
+
+    // Edit : function(item){
+    //   this.action = "update";
+    //   this.id = item.id;
+    //   this.team = item.team;
+    //   this.activity_yesterday = item.activity_yesterday;
+    //   this.activity_today = item.activity_today;
+    // },
+
+    Save : function(){
+      let conf = { headers: { "Authorization" : 'Bearer ' + this.key } };
+      this.$bvToast.show("loadingToast");
+      if(this.action === "insert"){
+        let form = new FormData();
+        form.append("id", this.id);
+        form.append("team", this.team);
+        form.append("activity_yesterday", this.activity_yesterday);
+        form.append("activity_today", this.activity_today);
+        form.append("problem_yesterday", this.problem_yesterday);
+        form.append("solution", this.solution);
+
+        this.axios.post("/daily", form, conf)
+        .then(response => {
+          this.$bvToast.hide("loadingToast");
+          if(this.search == ""){
+            this.getData();
+          } else {
+            this.searching();
+          }
+          this.message = response.data.message;
+          this.$bvToast.show("message");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      } else {
+        let form = {
+          team: this.team,
+          activity_yesterday: this.activity_yesterday,
+          activity_today: this.activity_today,
+          problem_yesterday: this.problem_yesterday,
+          solution: this.solution,
+        }
+        this.axios.put("/daily/" + this.id, form, conf)
+        .then(response => {
+          this.$bvToast.hide("loadingToast");
+          if(this.search == ""){
+            this.getData();
+          } else {
+            this.searching();
+          }
+          this.message = response.data.message;
+          this.$bvToast.show("message");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
+    },
+
+    Drop : function(id){
+      let conf = { headers: { "Authorization" : "Bearer " + this.key} };
+      if(confirm('Apakah anda yakin ingin menghapus data ini?')){
+        this.$bvToast.show("loadingToast");
+        this.axios.delete("/daily/" + id, conf)
+        .then(response => {
+            this.getData();
+            this.$bvToast.hide("loadingToast");
+            this.message = response.data.message;
+            this.$bvToast.show("message");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
+    },
+  },
+  mounted(){
+    this.key = localStorage.getItem("Authorization");
+    this.getData();
+  }
+}
+</script>
